7add47e6b1a40eb8c426906dd1ffc181a0004c8d
Andrei-Stefan Erhan <114987273+Andreliano@users.noreply.github.com>
2023-12-22 11:38:14 +0200

    fix: move day02 module in 2023 directory

--- 2023/day02/src/main/java/org/example/Main.java
+++ 2023/day02/src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Solver solver = new Solver();
-        Map<Integer, List<List<CubeInformation>>> games = solver.getAllLinesFromFile("day02/src/main/resources/input");
+        Map<Integer, List<List<CubeInformation>>> games = solver.getAllLinesFromFile("2023/day02/src/main/resources/input");
         System.out.println(solver.getSumOfAllPossibleGamesIdsCorrespondingWithExistingCubes(12,
                 13, 14,
                 games));
