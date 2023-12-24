94e78ca9680f1be2d137c73d15790d9b557fe659
Andrei-Stefan Erhan <114987273+Andreliano@users.noreply.github.com>
2023-12-24 11:50:56 +0200

    feat: add 2023 day04 part 2

--- 2023/day04/src/main/java/org/example/Main.java
+++ 2023/day04/src/main/java/org/example/Main.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         Solver solver = new Solver();
         Map<Integer, String> cards = solver.getAllCardsFromFile("2023/day04/src/main/resources/input");
+        Map<Integer, List<Integer>> firstCopies = solver.buildFirstCopiesMap(cards);
         System.out.println(solver.getTotalPointsFromAllCards(cards));
+        System.out.println(solver.getSumOfAllCardsAppearances(firstCopies, cards));
     }
 }
